c7dd5c184649310f0a537da8329e5ce5a4d2a875
Scott Love <scott.love@gmail.com>
2017-12-16 14:27:56 -0500

    update pipeline execute

--- 04_streaming/simulate/df02.py
+++ 04_streaming/simulate/df02.py
@@ -18,25 +18,23 @@ import apache_beam as beam
 import csv
 
 def addtimezone(lat, lon):
-   try:
-      import timezonefinder
-      tf = timezonefinder.TimezoneFinder()
-      tz = tf.timezone_at(lng=float(lon), lat=float(lat))
-      if tz is None:
-         tz = 'UTC'
-      return (lat, lon, tz)
-   except ValueError:
-      return (lat, lon, 'TIMEZONE') # header
+    try:
+        import timezonefinder
+        tf = timezonefinder.TimezoneFinder()
+        tz = tf.timezone_at(lng=float(lon), lat=float(lat))
+        if tz is None:
+            tz = 'UTC'
+        return lat, lon, tz
+        except ValueError:
+            return (lat, lon, 'TIMEZONE') # header
+
 
 if __name__ == '__main__':
-   pipeline = beam.Pipeline('DirectRunner')
-
-   airports = (pipeline 
-      | beam.io.ReadFromText('airports.csv.gz')
-      | beam.Map(lambda line: next(csv.reader([line])))
-      | beam.Map(lambda fields: (fields[0], addtimezone(fields[21], fields[26])))
-   )
-
-   airports | beam.Map(lambda (airport, data): '{},{}'.format(airport, ','.join(data)) )| beam.io.textio.WriteToText('airports_with_tz')
-
-   pipeline.run()
+    with beam.Pipeline('DirectRunner') as pipeline:
+        airports = (pipeline
+                    | beam.io.ReadFromText('airports.csv.gz')
+                    | beam.Map(lambda line: next(csv.reader([line])))
+                    | beam.Map(lambda fields: (fields[0], addtimezone(fields[21], fields[26])))
+                    )
+        airports | beam.Map(lambda (airport, data): '{},{}'.format(airport, ','.join(data)) )| beam.io.textio.WriteToText('airports_with_tz')
+        pipeline.run()
